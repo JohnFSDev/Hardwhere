b6dc42bc7b8139e9aa3d5de4f5b31047fda994ab
Jean Azar <jeancarlosazar@gmail.com>
2023-12-21 12:19:36 -0400

    Bootsrap + NavBar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PortalVue from 'portal-vue'
 
 createApp(App).mount('#app')
+App.use(PortalVue)
+App.mount('#app')
