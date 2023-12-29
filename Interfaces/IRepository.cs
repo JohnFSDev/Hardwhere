62fe07aef4df354fee1ae3af7ce13ef0101f2adb
JOHN LUIS DEL ROSARIO SANCHEZ <johnluis22@outlook.com>
2023-12-28 18:43:04 -1100

    Metodos get para los componentes Power Supply y Ram.

--- Interfaces/IRepository.cs
+++ Interfaces/IRepository.cs
@@ -3,7 +3,6 @@
     public interface IRepository<TGeneric> where TGeneric : class
     {
         void Create(TGeneric generic);
-        TGeneric GetComponentsById(int id);
 
         IEnumerable<TGeneric> GetAllComponents();
 
