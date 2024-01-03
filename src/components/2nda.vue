987f770fbddfa53c96f86be1394c76ca6dfef8fd
Jean Azar <jeancarlosazar@gmail.com>
2024-01-03 10:31:59 -0400

    Front-Main

---
+++ src/components/2nda.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-toolbar>
+    <v-app-bar color="#FFB703" :elevation="0">
+      <img src="/hw_logo.png" height="270" />
+      <v-card
+      class="mx-auto"
+      color="grey-lighten-3"
+      max-width="700"
+    >
+      <v-card-text>
+        <v-text-field
+          :loading="loading"
+          density="compact"
+          variant="solo"
+          label="Buscar"
+          append-inner-icon="mdi-magnify"
+          single-line
+          hide-details
+          style="width: 600px;"
+          @click:append-inner="onClick"
+        ></v-text-field>
+      </v-card-text>
+    </v-card>
+    <a href="#">Que es Hardwhere</a>
+    </v-app-bar>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      loaded: false,
+      loading: false,
+    }),
+
+    methods: {
+      onClick () {
+        this.loading = true
+
+        setTimeout(() => {
+          this.loading = false
+          this.loaded = true
+        }, 2000)
+      },
+    },
+  }
+</script>
