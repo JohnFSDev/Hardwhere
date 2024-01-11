99ea8020fd1014d77238d4b613e4ce6762140d72
Jean Azar <jeancarlosazar@gmail.com>
2024-01-11 16:19:49 -0400

    Pantalla /Resultado - Pendiente 3 v-card parte inferior

--- src/components/Resultado.vue
+++ src/components/Resultado.vue
@@ -1,95 +1,95 @@
+
 <template>
-
-<v-container>
-
-  <v-card elevation="0">
-    <v-row no-gutters>
-      <!-- Columna para el contenedor de la imagen -->
-      <v-col cols="4">
-        <v-card class="image-container">
-          <v-img src="/computer-ram.jpg" aspect-ratio="1"></v-img>
-        </v-card>
-      </v-col>
-
-      <!-- Columna para el contenido -->
-      <v-col cols="8">
-        <v-card-title class="custom-title">{{component.title}}</v-card-title>
-        <v-card-text>
-          {{component.description}}
-        </v-card-text>
-        <v-card-text>
-          Marca
-        </v-card-text>
-
-        <v-card-text>
-          Ejemplo 1
-        </v-card-text>
-
-        <v-card-text>
-          Ejemplo 2
-        </v-card-text>
-      </v-col>
-    </v-row>
-  </v-card>
-
-</v-container>
-
-
-<v-container>
-
-  <v-divider class="my-7"></v-divider>
-
-  <!-- <v-row justify="center">
-    <v-col cols="4" v-for="i in 3" :key="i">
-      <v-card class="image-2nd">
-        <v-row no-gutters>
+  <v-container fluid style="background-color: #f4f4f6;">
+    <v-container>
+      <v-card elevation="0" style="background-color: #f4f4f6;">
+        <v-row align="center">
+          <!-- Columna para el contenedor de la imagen -->
           <v-col cols="4">
-            <v-card class="image-2nd">
+            <v-card class="image-container">
               <v-img src="/computer-ram.jpg" aspect-ratio="1"></v-img>
             </v-card>
           </v-col>
 
-          <v-col cols="8">
-            <v-card-title class="custom-title" style="color: black;">Ejemplo de Título 1</v-card-title>
-            <v-card-text>
-              Descripción del primer ejemplo de componente.
+          <!-- Columna para el contenido -->
+          <v-col cols="8" class="mt-2">
+            <v-card-title class="custom-title">ASUS Dual Geforce RTX 4060</v-card-title>
+            <v-card-text class="limited-width">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dictum facilisis dolor. Vestibulum vehicula dignissim libero, viverra convallis risus tempus nec. Nullam aliquet odio vel risus rhoncus, a convallis leo sollicitudin. Sed eu purus neque. Sed ultricies ligula ut 
+            </v-card-text>
+            <v-card-text style="font-size: 22px;">
+              Marca: ASUS
+            </v-card-text>
+          
+            <v-card-text style="font-size: 22px;">
+              Graphic Coprocessor: Nvidia Geforce 4060
+            </v-card-text>
+          
+            <v-card-text style="font-size: 22px;">
+              Graphics Ram: 2500mhz
             </v-card-text>
           </v-col>
         </v-row>
       </v-card>
-    </v-col>
-  </v-row> -->
-</v-container>
+    </v-container>
 
+    <v-container>
+      <v-divider class="my-7"></v-divider>
 
+      <v-row justify="center">
+        <v-col cols="4" v-for="i in 3" :key="i">
+          <v-card class="image-2nd">
+            <v-row no-gutters>
+              <v-col cols="4">
+                <v-card class="image-2nd">
+                  <v-img src="/computer-ram.jpg" aspect-ratio="1"></v-img>
+                </v-card>
+              </v-col>
+
+              <v-col cols="8">
+                <v-card-title style="color: black;">Ejemplo de Título 1</v-card-title>
+                <v-card-text>
+                  Descripción del primer ejemplo de componente.
+                </v-card-text>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
 </template>
 
 <style>
-
-@import url('https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@700&display=swap');
 
   .custom-title {
+    font-size: 30px !important;
     color: #023047;
-    font-family: 'Zen Maru Gothic', sans-serif;
-    font-size: 40px;
+    font-family: 'Zen Maru Gothic', sans-serif; 
   }
 
-.image-2nd {
-  width: 150px;
-  height: 150px;
-}
+  .image-2nd {
+    width: 150px;
+    height: 150px;
+  }
 
-.image-container {
-  width: 350px; /* Define el ancho del cuadro de imagen */
-  height: 350px; /* Define la altura del cuadro de imagen */
-  border-radius: 12px; /* Bordes curvos */
-  overflow: hidden; /* Recorta el contenido que se desborda */
-}
+  .image-container {
+    width: 400px;
+    height: 400px;
+    border-radius: 12px;
+    overflow: hidden;
+  }
 
-.rounded-image {
-  border-radius: 12px; /* Bordes curvos para la imagen */
-}
+  .rounded-image {
+    border-radius: 12px;
+  }
 
+  .limited-width {
+    max-width: 650px;
+    font-size: 20px !important;
+    margin-top: 20px;
+  }
 </style>
 
 <script>
